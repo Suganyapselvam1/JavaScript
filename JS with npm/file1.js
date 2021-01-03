de4001b4b73c406647c52dcf232990f3c31d65fa
BC226H2-OOBE-01\venkat <suganyapselvam@gmail.com>
2021-01-03 20:16:58 +0530

    adding npm lite-server and basic JS

---
+++ JS with npm/file1.js
@@ -0,0 +1,12 @@
+function myfuction(){
+    document.write("venkay baby");
+    console.log("sugu baby");
+    //window.alert("i am changin the content of the demo 1");
+    document.getElementById("demo1").innerHTML="paragraph changed";
+}
+function myfunc(){
+    var x=5;
+    var y=6;
+    z=x+y;
+    alert("the value of z is: "+z+".");
+}
